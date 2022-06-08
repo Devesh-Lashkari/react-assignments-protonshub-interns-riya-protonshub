8f8592ae1feb75ebe7a7cdaeefe652b278995b1c
Riya Gandhi <riya090401@gmail.com>
2022-06-08 14:30:27 +0530

    fix main.js

--- responsive html form/main.js
+++ responsive html form/main.js
@@ -1,4 +1,7 @@
 function addData() {
+
+  var table = document.getElementById("response-table");
+
   var firstName = document.getElementById("fname").value;
   var lastName = document.getElementById("lname").value;
   var emailAddress = document.getElementById("mail").value;
@@ -9,45 +12,41 @@ function addData() {
   var state = document.getElementById("state").value;
   var country = document.getElementById("country").value;
 
-  var newRow = document.createElement("tr");
-  var newCell = document.createElement("td");
+  var newRow = table.insertRow();
 
-  newCell.innerHTML = firstName;
-  newRow.append(newCell);
-
-  newCell.innerHTML = lastName;
-  newRow.append(newCell);
-
-  newCell.innerHTML = emailAddress;
-  newRow.append(newCell);
-
-  newCell.innerHTML = mobileNumber;
-  newRow.append(newCell);
-
-  newCell.innerHTML = homeAddress;
-  newRow.append(newCell);
-
-  newCell.innerHTML = city;
-  newRow.append(newCell);
-
-  newCell.innerHTML = pincode;
-  newRow.append(newCell);
-
-  newCell.innerHTML = state;
-  newRow.append(newCell);
-
-  newCell.innerHTML = country;
-  newRow.append(newCell);
-
-  document.getElementById("rows").appendChild(newRow);
-
+  var newCell0 = newRow.insertCell();
+  newCell0.innerHTML = firstName;
   document.getElementById("fname").value = "";
+
+  var newCell1 = newRow.insertCell();
+  newCell1.innerHTML = lastName;
   document.getElementById("lname").value = "";
+
+  var newCell2 = newRow.insertCell();
+  newCell2.innerHTML = emailAddress;
   document.getElementById("mail").value = "";
+
+  var newCell3 = newRow.insertCell();
+  newCell3.innerHTML = mobileNumber;
   document.getElementById("mobile").value = "";
+
+  var newCell4 = newRow.insertCell();
+  newCell4.innerHTML = homeAddress;
   document.getElementById("haddress").value = "";
+
+  var newCell5 = newRow.insertCell();
+  newCell5.innerHTML = city;
   document.getElementById("city").value = "";
+
+  var newCell6 = newRow.insertCell();
+  newCell6.innerHTML = pincode;
   document.getElementById("pincode").value = "";
+
+  var newCell7 = newRow.insertCell();
+  newCell7.innerHTML = state;
   document.getElementById("state").value = "";
+
+  var newCell8 = newRow.insertCell();
+  newCell8.innerHTML = country;
   document.getElementById("country").value = "";
 }
